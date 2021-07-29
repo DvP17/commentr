fd96b95d87db0f27846ac7f23c25b298baa1fc81
dvp17 <dvp17@protonmail.ch>
2021-07-29 10:51:00 +0200

    added functions comr_h3 & comr_h4; added argument to declare type of filler (set default as white space); compatible now with RStudio structuring of TOC

--- R/comr.R
+++ R/comr.R
@@ -1,4 +1,4 @@
-#' Centered Comments for R
+#' Headline 1 comment for R
 #'
 #' Gives centered comments. Always capitalized.
 #'
@@ -54,7 +54,7 @@ comr_h1 <- function(title, width = 80){
 }
 
 
-#' Comments for R
+#' Headline 2 comment for R
 #'
 #' Gives comments with left indentation. Without capitalization when arg2 is
 #' missing.
@@ -65,18 +65,20 @@ comr_h1 <- function(title, width = 80){
 #' @param width Number of characters (integer) for width of line. Default is 80.
 #' @param align String for alignment of title. Default is "right". Other options are "left" and "center".
 #' @param capit If title should be in capitals. True or false.
+#' @param fill String for type of filler. Default is " " for white space. Can be any character (e.g., "#").
 #'
 #' @examples comr_h2("Headline 1")
 #'
 #' @export
-comr_h2 <- function(title, width = 80, align = "right", capit = FALSE){
+comr_h2 <- function(title, width = 80, align = "right", capit = FALSE,
+                    fill = " "){
   # Number of title characters
   tit_n <- nchar(title)
 
   # Width is one character less than margin
   width <- width - 1
 
-  # Declare filler
+  # Declare filler number
   filler <- width - tit_n - 2
   if((filler %% 2) == 0) {
     fill1 <- filler / 2
@@ -86,6 +88,9 @@ comr_h2 <- function(title, width = 80, align = "right", capit = FALSE){
     fill2 <- (filler - 1) / 2 + 1
   }
 
+  # Declare filler type
+  fill_type <- fill
+
   # Capitalize
   if (capit){
     title <- toupper(title)
@@ -94,16 +99,16 @@ comr_h2 <- function(title, width = 80, align = "right", capit = FALSE){
   # Generate output
   if (align == "left") {
     out <- paste(c(rep("#", width), "|", "\n",
-                   rep("#", 2), " ", title, " ",
-                   rep("#", filler - 2)), collapse = "")
+                   "#", " ", title, " ", rep(fill_type, filler - 5), "####"),
+                 collapse = "")
   } else if (align == "right") {
     out <- paste(c(rep("#", width), "|", "\n",
-                   rep("#", filler - 4), " ", title, " ",
-                   rep("#", 4)), collapse = "")
+                   "#", rep(fill_type, filler - 5), " ", title, " ", "####"),
+                 collapse = "")
   } else if (align == "center") {
     out <- paste(c(rep("#", width), "|", "\n",
-                   rep("#", fill1), " ", title, " ",
-                   rep("#", fill2)), collapse = "")
+                   "#", rep(fill_type, fill1 - 1), " ", title, " ",
+                   rep(fill_type, fill2 - 4), "####"), collapse = "")
   }
 
   # Write to Clipboard
@@ -123,3 +128,148 @@ comr_h2 <- function(title, width = 80, align = "right", capit = FALSE){
 }
 
 
+#' Headline 3 comment for R
+#'
+#' Gives comments with left indentation. Without capitalization when arg2 is
+#' missing.
+#'
+#' @usage comr_h3(arg1 = "Headline 1", capit = FALSE)
+#'
+#' @param title String for title.
+#' @param width Number of characters (integer) for width of line. Default is 80.
+#' @param align String for alignment of title. Default is "right". Other options are "left" and "center".
+#' @param capit If title should be in capitals. True or false.
+#' @param fill String for type of filler. Default is " " for white space. Can be any character (e.g., "#").
+#'
+#' @examples comr_h3("Headline 1")
+#'
+#' @export
+comr_h3 <- function(title, width = 80, align = "right", capit = FALSE,
+                    fill = " "){
+  # Number of title characters
+  tit_n <- nchar(title)
+
+  # Width is one character less than margin
+  width <- width - 1
+
+  # Declare filler number
+  filler <- width - tit_n - 2
+  if((filler %% 2) == 0) {
+    fill1 <- filler / 2
+    fill2 <- filler / 2
+  } else {
+    fill1 <- (filler - 1) / 2
+    fill2 <- (filler - 1) / 2 + 1
+  }
+
+  # Declare filler type
+  fill_type <- fill
+
+  # Capitalize
+  if (capit){
+    title <- toupper(title)
+  }
+
+  # Generate output
+  if (align == "left") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "##", " ", title, " ", rep(fill_type, filler - 6), "####"),
+                 collapse = "")
+  } else if (align == "right") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "##", rep(fill_type, filler - 6), " ", title, " ", "####"),
+                 collapse = "")
+  } else if (align == "center") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "##", rep(fill_type, fill1 - 2), " ", title, " ",
+                   rep(fill_type, fill2 - 4), "####"), collapse = "")
+  }
+
+  # Write to Clipboard
+  choice <- menu(c("Yes", "No"),
+                 title="Do you want to copy into your clipboard?")
+
+  if (choice == 1) {
+
+    writeClipboard(out)
+
+  } else {
+
+    return(cat(out))
+
+  }
+
+}
+
+#' Headline 4 comment for R
+#'
+#' Gives comments with left indentation. Without capitalization when arg2 is
+#' missing.
+#'
+#' @usage comr_h4(arg1 = "Headline 1", capit = FALSE)
+#'
+#' @param title String for title.
+#' @param width Number of characters (integer) for width of line. Default is 80.
+#' @param align String for alignment of title. Default is "right". Other options are "left" and "center".
+#' @param capit If title should be in capitals. True or false.
+#' @param fill String for type of filler. Default is " " for white space. Can be any character (e.g., "#").
+#'
+#' @examples comr_h4("Headline 1")
+#'
+#' @export
+comr_h4 <- function(title, width = 80, align = "right", capit = FALSE,
+                    fill = " "){
+  # Number of title characters
+  tit_n <- nchar(title)
+
+  # Width is one character less than margin
+  width <- width - 1
+
+  # Declare filler number
+  filler <- width - tit_n - 2
+  if((filler %% 2) == 0) {
+    fill1 <- filler / 2
+    fill2 <- filler / 2
+  } else {
+    fill1 <- (filler - 1) / 2
+    fill2 <- (filler - 1) / 2 + 1
+  }
+
+  # Declare filler type
+  fill_type <- fill
+
+  # Capitalize
+  if (capit){
+    title <- toupper(title)
+  }
+
+  # Generate output
+  if (align == "left") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "###", " ", title, " ", rep(fill_type, filler - 7), "####"),
+                 collapse = "")
+  } else if (align == "right") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "###", rep(fill_type, filler - 7), " ", title, " ", "####"),
+                 collapse = "")
+  } else if (align == "center") {
+    out <- paste(c(rep("#", width), "|", "\n",
+                   "###", rep(fill_type, fill1 - 3), " ", title, " ",
+                   rep(fill_type, fill2 - 4), "####"), collapse = "")
+  }
+
+  # Write to Clipboard
+  choice <- menu(c("Yes", "No"),
+                 title="Do you want to copy into your clipboard?")
+
+  if (choice == 1) {
+
+    writeClipboard(out)
+
+  } else {
+
+    return(cat(out))
+
+  }
+
+}
